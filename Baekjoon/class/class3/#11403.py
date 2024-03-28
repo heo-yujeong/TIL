a8bab04c34031757ebc3be54a436dc28b4721f04
heo-yujeong <yu.jeong5899@gmail.com>
2024-03-28 21:46:02 +0900

    24.03.28

---
+++ Baekjoon/class/class3/#11403.py
@@ -0,0 +1,13 @@
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+for line in graph:
+    for num in line:
+        print(num, end=' ')
+    print()
