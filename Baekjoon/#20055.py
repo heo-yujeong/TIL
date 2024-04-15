01c7e780e685aae61e5d6ebd4361206bf8f1db5a
heo-yujeong <yu.jeong5899@gmail.com>
2024-04-15 22:45:11 +0900

    24.04.15

--- Baekjoon/#20055.py
+++ Baekjoon/#20055.py
@@ -1,9 +1,32 @@
-N, K = map(int, input().split())
-Ai = list(map(int, input().split()))
+import sys
+from collections import deque
+input = sys.stdin.readline
 
-step = 1
+N, K = map(int, input().split())
+Ai = deque(map(int, input().split()))
+robot = deque([0] * N)
+
+step = 0
 
 while True:
-    if step == K:
+    step += 1
+
+    Ai.rotate(1)
+    robot.rotate(1)
+    robot[N-1] = 0
+
+    for i in range(N-2, -1, -1):
+        if Ai[i+1] > 0 and robot[i+1] == 0 and robot[i] == 1:
+            robot[i+1] = 1
+            robot[i] = 0
+            Ai[i+1] -= 1
+    robot[N-1] = 0
+
+    if Ai[0] >= 1 and robot[0] == 0:
+        robot[0] = 1
+        Ai[0] -= 1
+
+    if Ai.count(0) >= K:
         break
-    
+
+print(step)
