21f965462aa11f7cda25aabc514852a532ca0e16
heo-yujeong <yu.jeong5899@gmail.com>
2024-05-01 17:04:54 +0900

    24.05.01_re

--- Baekjoon/#1421.py
+++ Baekjoon/#1421.py
@@ -1,30 +1,23 @@
-import sys
-sys.stdin = open('input.txt')
+# N: 나무개수, C: 자르는 비용, W: 나무 한단위 가격
+# 길이가 L일때 벌 수 있는 금액 = K * L * W
+N, C, W = map(int, input().split())
+length = [int(input()) for _ in range(N)]
 
-T = int(input())
+max_money = 0
 
-for _ in range(T):
-    # N: 나무개수, C: 자르는 비용, W: 나무 한단위 가격
-    # 길이가 L일때 벌 수 있는 금액 = K * L * W
-    N, C, W = map(int, input().split())
-    length = [int(input()) for _ in range(N)]
+for l in range(1, max(length)+1):
+    money_sum = 0
+    for tree in length:
+        if tree % l == 0:
+            c = (tree//l-1) * C
+        else:
+            c = tree//l * C
+        money = (tree//l * l * W) - c
+        if money < 0:
+            continue
+        money_sum += money
 
-    max_money = 0
+    if max_money < money_sum:
+        max_money = money_sum
 
-    for l in range(1, max(length)+1):
-        money_sum = 0
-        for tree in length:
-            if tree % l == 0:
-                c = (tree//l-1) * C
-            else:
-                c = tree//l * C
-            money = (tree//l * l * W) - c
-
-            if money < 0:
-                continue
-            money_sum += money
-
-        if max_money < money_sum:
-            max_money = money_sum
-
-    print(max_money)
+print(max_money)
