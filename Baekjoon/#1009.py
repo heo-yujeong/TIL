8b80d430fb04a497213d10e89742c458168dec01
heo-yujeong <yu.jeong5899@gmail.com>
2024-08-15 20:15:13 +0900

    24.08.15

---
+++ Baekjoon/#1009.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    a %= 10
+
+    if a == 0:
+        print(10)
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        if b % 2 == 1:
+            print(a)
+        else:
+            print(a ** 2 % 10)
+    else:
+        if b % 4 == 0:
+            print(a ** 4 % 10)
+        else:
+            print(a ** (b % 4) % 10)
