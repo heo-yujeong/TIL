08aadbdf6ea832c51fcf36f324652eed7e4e1b14
heo-yujeong <yu.jeong5899@gmail.com>
2024-09-24 22:10:33 +0900

    24.09.24

---
+++ Baekjoon/#30031.py
@@ -0,0 +1,16 @@
+N = int(input())
+result = 0
+
+for _ in range(N):
+    w, h = map(int, input().split())
+
+    if w == 136:
+        result += 1000
+    elif w == 142:
+        result += 5000
+    elif w == 148:
+        result += 10000
+    else:
+        result += 50000
+
+print(result)
