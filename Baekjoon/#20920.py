b31958877184d2449bd20e2fd73771a2323e6f26
허유정 <yu.jeong5899@gmail.com>
2024-12-27 20:22:18 +0900

    24.12.27

---
+++ Baekjoon/#20920.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+dict = {}
+
+for _ in range(N):
+    word = input().rstrip()
+
+    if len(word) < M:
+        continue
+    else:
+        if word in dict:
+            dict[word] += 1
+        else:
+            dict[word] = 1
+
+sorted_dict = sorted(dict.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+
+for i in sorted_dict:
+    print(i[0])
