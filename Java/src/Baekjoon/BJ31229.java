41bf8ff879e6523f71ed881f81c8aaad63d6a835
허유정 <yu.jeong5899@gmail.com>
2025-05-19 22:11:45 +0900

    25.05.19

---
+++ Java/src/Baekjoon/BJ31229.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ31229 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(2 * i + 1).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
