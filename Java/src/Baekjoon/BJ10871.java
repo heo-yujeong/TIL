5a4d710a4c57814d4223bdbe693b71801c9135ee
허유정 <yu.jeong5899@gmail.com>
2025-07-03 23:06:28 +0900

    25.07.03

---
+++ Java/src/Baekjoon/BJ10871.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (a < X) sb.append(a + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
