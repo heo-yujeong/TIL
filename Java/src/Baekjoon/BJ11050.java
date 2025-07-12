35b6063728d1a3ea4874e082620d3f9e88a3f910
허유정 <yu.jeong5899@gmail.com>
2025-07-12 15:45:48 +0900

    25.07.12

---
+++ Java/src/Baekjoon/BJ11050.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11050 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        System.out.println(factorial(N) / (factorial(N-K) * factorial(K)));
+    }
+
+    private static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n-1);
+    }
+}
