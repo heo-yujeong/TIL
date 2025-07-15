9051b98679c8518945f763a4b4bb15a960d89ead
허유정 <yu.jeong5899@gmail.com>
2025-07-15 22:42:29 +0900

    25.07.15

---
+++ Java/src/Baekjoon/BJ2609.java
@@ -0,0 +1,28 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int G = gcd(A, B);
+
+        System.out.println(G);
+        System.out.println(A * B / G);
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
