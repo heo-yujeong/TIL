0b437f062f695e71632bbd32227269e75c15f9df
허유정 <yu.jeong5899@gmail.com>
2025-08-25 20:23:39 +0900

    25.08.25

---
+++ Java/src/Baekjoon/BJ2075_2.java
@@ -0,0 +1,30 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BJ2075_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < N-1; i++) {
+            pq.remove();
+        }
+
+        System.out.println(pq.poll());
+    }
+}
