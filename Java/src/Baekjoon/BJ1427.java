8c5cfe9cb215545899feb1cf3695096632109c81
허유정 <yu.jeong5899@gmail.com>
2025-09-13 09:50:59 +0900

    25.09.13

---
+++ Java/src/Baekjoon/BJ1427.java
@@ -0,0 +1,18 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] N = br.readLine().toCharArray();
+
+        Arrays.sort(N);
+        for (int i = N.length - 1; i >= 0; i--) {
+            System.out.print(N[i]);
+        }
+    }
+}
