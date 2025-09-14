606e8ad569655bcfb2eb8f0d5288f90c4c9194e2
허유정 <yu.jeong5899@gmail.com>
2025-09-14 19:56:54 +0900

    25.09.14

---
+++ Java/src/Baekjoon/BJ9063.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ9063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] x = new int[N];
+        int[] y = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (N == 1) {
+            System.out.println(0);
+        } else {
+            Arrays.sort(x);
+            Arrays.sort(y);
+            System.out.println((x[N-1] - x[0]) * (y[N-1] - y[0]));
+        }
+    }
+}
