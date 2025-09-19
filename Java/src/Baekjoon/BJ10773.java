fa89a1ed1dcc1e867a241c964eeb04271c13dcc2
허유정 <yu.jeong5899@gmail.com>
2025-09-19 16:53:22 +0900

    25.09.19

---
+++ Java/src/Baekjoon/BJ10773.java
@@ -0,0 +1,32 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BJ10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        for (int s : stack) {
+            sum += s;
+        }
+
+        System.out.println(sum);
+    }
+}
