0446e7efd0540662e881e01afd56b2b1e972d3de
허유정 <yu.jeong5899@gmail.com>
2025-09-22 15:51:25 +0900

    25.09.22

---
+++ Java/src/Baekjoon/BJ1269.java
@@ -0,0 +1,45 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class BJ1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> A = new HashSet<>();
+        HashSet<Integer> B = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int cnt = 0;
+        for (Integer a : A) {
+            if (!B.contains(a)) {
+                cnt++;
+            }
+        }
+
+        for (Integer b : B) {
+            if (!A.contains(b)) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
