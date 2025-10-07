cc549897230b84f91c2832abab8d019bc487c578
허유정 <yu.jeong5899@gmail.com>
2025-10-07 12:59:21 +0900

    25.10.07

---
+++ Java/src/Baekjoon/BJ24313.java
@@ -0,0 +1,28 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ24313 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int n0 = Integer.parseInt(st.nextToken());
+
+        if (a1 * n0 + a0 <= c * n0 && c >= a1) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
