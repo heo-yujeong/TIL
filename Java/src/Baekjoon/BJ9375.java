92c1a4abc94223452ea3e3e1f41c1f02e9c9dc42
허유정 <yu.jeong5899@gmail.com>
2025-11-03 19:43:40 +0900

    25.11.03

---
+++ Java/src/Baekjoon/BJ9375.java
@@ -0,0 +1,39 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BJ9375 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            Map<String, Integer> map = new HashMap<>();
+
+            int n = Integer.parseInt(br.readLine());
+            while (n-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                st.nextToken();
+
+                String kind = st.nextToken();
+                map.put(kind, map.getOrDefault(kind, 0) + 1);
+            }
+
+            int cnt = 1;
+            for (int val : map.values()) {
+                cnt *= (val + 1);
+            }
+
+            sb.append(cnt - 1).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
