329b3de3a6b4548497dd364a633b8b63004f1821
허유정 <yu.jeong5899@gmail.com>
2025-11-11 11:50:41 +0900

    25.11.11

---
+++ Java/src/Baekjoon/BJ11721.java
@@ -0,0 +1,23 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            sb.append(input.charAt(i));
+            if ((i+1) % 10 == 0) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
