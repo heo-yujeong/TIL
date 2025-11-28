a137f351b727e85f9bdf16fdb70fcae0c1f0ef56
허유정 <yu.jeong5899@gmail.com>
2025-11-28 22:21:37 +0900

    25.11.28

---
+++ Java/src/Baekjoon/BJ28235.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ28235 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        if (input.equals("SONGDO")) {
+            System.out.println("HIGHSCHOOL");
+        } else if (input.equals("CODE")) {
+            System.out.println("MASTER");
+        } else if (input.equals("2023")) {
+            System.out.println("0611");
+        } else {
+            System.out.println("CONTEST");
+        }
+    }
+}
