8a8da4685c946c24b866f0575ae522ace0950e9c
허유정 <yu.jeong5899@gmail.com>
2025-12-05 21:05:17 +0900

    25.12.05

---
+++ Java/src/Baekjoon/BJ16693.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ16693 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double A1 = Double.parseDouble(st.nextToken());
+        double P1 = Double.parseDouble(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        double R1 = Double.parseDouble(st.nextToken());
+        double P2 = Double.parseDouble(st.nextToken());
+
+        if (A1 / P1 > (R1 * R1 * Math.PI) / P2) {
+            System.out.println("Slice of pizza");
+        } else {
+            System.out.println("Whole pizza");
+        }
+    }
+}
