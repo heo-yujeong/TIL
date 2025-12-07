aeeba74d5a46a124d37bc5ec1db8bf6b485bf761
허유정 <yu.jeong5899@gmail.com>
2025-12-07 13:03:10 +0900

    25.12.07

---
+++ Java/src/Baekjoon/BJ9325.java
@@ -0,0 +1,33 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ9325 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int s = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int p = Integer.parseInt(st.nextToken());
+                int q = Integer.parseInt(st.nextToken());
+
+                s += (p * q);
+            }
+
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
