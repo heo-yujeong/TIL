ee98d2bd5e043f92181884edb1c1395c9c224015
허유정 <yu.jeong5899@gmail.com>
2025-12-27 16:33:26 +0900

    25.12.27

---
+++ Java/src/Baekjoon/BJ16504.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ16504 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        long sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
