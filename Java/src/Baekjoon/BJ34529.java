7b6e02bfd1445c0ee3ff7ab3cb8aae7554ab9939
허유정 <yu.jeong5899@gmail.com>
2026-01-02 19:28:58 +0900

    26.01.02

---
+++ Java/src/Baekjoon/BJ34529.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ34529 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int Z = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int U = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        System.out.println(U / 100 * X + V / 50 * Y + W / 20 * Z);
+    }
+}
