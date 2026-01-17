d76cb94e6f4075cf9892c5e9cb8312ef62b3ee35
허유정 <yu.jeong5899@gmail.com>
2026-01-17 19:49:44 +0900

    26.01.17

---
+++ Java/src/Baekjoon/BJ17874.java
@@ -0,0 +1,19 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ17874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.max(h, n-h) * Math.max(v, n-v) * 4);
+    }
+}
