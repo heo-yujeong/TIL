25b1afea4722ff9753b89f256056cbce3c9e2684
허유정 <yu.jeong5899@gmail.com>
2026-01-28 20:15:05 +0900

    26.01.28

---
+++ Java/src/Baekjoon/BJ11283.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ11283 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char input = br.readLine().charAt(0);
+        System.out.println((int)input - 44031);
+    }
+}
