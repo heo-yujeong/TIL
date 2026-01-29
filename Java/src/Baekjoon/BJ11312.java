3b61e07b2d1c7bba011e96220712e9f6281b561a
허유정 <yu.jeong5899@gmail.com>
2026-01-29 19:49:54 +0900

    26.01.29

---
+++ Java/src/Baekjoon/BJ11312.java
@@ -0,0 +1,22 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11312 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            long A = Long.parseLong(st.nextToken());
+            long B = Long.parseLong(st.nextToken());
+
+            System.out.println(A / B * A / B);
+        }
+    }
+}
