96ea24fa9afa289cb2eb16971e2d2025dba9c18d
허유정 <yu.jeong5899@gmail.com>
2026-02-25 22:34:29 +0900

    26.02.25

---
+++ Java/src/Baekjoon/BJ28281.java
@@ -0,0 +1,29 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ28281 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken()) * X;
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N-1; i++) {
+            min = Math.min(min, A[i] + A[i+1]);
+        }
+
+        System.out.println(min);
+    }
+}
