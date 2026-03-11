b67d824c8dd78ca460e9425a3f10bde8fe7ea7b1
허유정 <yu.jeong5899@gmail.com>
2026-03-11 22:28:41 +0900

    26.03.11

---
+++ Java/src/Baekjoon/BJ27434.java
@@ -0,0 +1,26 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BJ27434 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(1, N));
+    }
+
+    private static BigInteger factorial(int a, int n) {
+        BigInteger num = BigInteger.valueOf(a);
+
+        if (a < n) {
+            int b = (a + n) / 2;
+            num = factorial(a, b).multiply(factorial(b + 1, n));
+        }
+
+        return num;
+    }
+}
