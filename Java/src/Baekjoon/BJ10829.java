c122f7a2b9488e7c38167023ef7a0d84f8c8336f
허유정 <yu.jeong5899@gmail.com>
2026-03-21 21:44:13 +0900

    26.03.21

---
+++ Java/src/Baekjoon/BJ10829.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10829 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        System.out.println(Long.toBinaryString(N));
+    }
+}
