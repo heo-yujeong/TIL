c7e448d895d72c893fc3595e74e4bd7ae9e68cc4
허유정 <yu.jeong5899@gmail.com>
2026-04-07 23:22:42 +0900

    26.04.07

---
+++ Java/src/Baekjoon/BJ2083.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2083 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+
+            sb.append(name).append(" ");
+            if (age > 17 || weight >= 80) {
+                sb.append("Senior").append("\n");
+            } else {
+                sb.append("Junior").append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
